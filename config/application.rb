76b03194d8f0588e16cda5e0c9fb5318cf966b53
LindseySawa <20betternbetter21@gmail.com>
2019-08-09 16:40:21 +0900

    updating files

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module ChatSpace
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    end
   end
 end
