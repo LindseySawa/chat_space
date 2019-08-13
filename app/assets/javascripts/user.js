8456550318a8a5957f57359c3a60485e187d4fd4
LindseySawa <20betternbetter21@gmail.com>
2019-08-13 19:18:07 +0900

    deleting

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -48,4 +48,30 @@ $(function () {
 
   });
 
+
+  $(function () {
+    function appendUsers(user_id, user_name) {
+      var html = `<div class='chat-group-user'>
+                    <input name='group[user_ids][]' type='hidden' value=${user_id}>
+                    <p class='chat-group-user__name'>${user_name}</p>
+                    <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                  </div>`;
+      return html;
+    }
+
+    $('#user-search-result').on('click', ".chat-group-user__btn--add", function () {
+      var user_id = $(this).attr("data-user-id");
+      var user_name = $(this).attr("data-user-name");
+      var html = appendUsers(user_id, user_name);
+      $('#chat_member').append(html);
+      $(this).parent().remove();
+
+    })
+  })
+  $(function () {
+    $('.chat-group-user').on('click', ".user-search-remove", function () {
+      $(this).parent().remove();
+    })
+  })
+
 })
